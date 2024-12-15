9514fc4f4b2ac43b06347351c1e7ebed4dd83679
Kavinda Ravihansa <164628196+kavinda99r@users.noreply.github.com>
2024-12-15 22:13:54 +0530

    Updated SignUp.jsx

--- frontend/note-app/src/pages/SignUp/SignUp.jsx
+++ frontend/note-app/src/pages/SignUp/SignUp.jsx
@@ -68,7 +68,7 @@ const SignUp = () => {
       <div className="flex items-center justify-center mt-20">
         <div className="w-96 border rounded bg-white px-7 py-10 drop-shadow">
           <form onSubmit={handleSignUp}>
-            <h4 className="text-2xl mb-7">SignUP</h4>
+            <h4 className="text-2xl mb-7">Signup</h4>
 
             <input
               type="text"
